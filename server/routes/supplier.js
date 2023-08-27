fd47638c7918732f2cd9a8278fac342c77dbc6e8
mhslekbar <83383812+mhslekbar@users.noreply.github.com>
2023-08-27 19:27:43 +0000

    RETOUCH BACKEND

--- server/routes/supplier.js
+++ server/routes/supplier.js
@@ -3,10 +3,11 @@ const {
   getSuppliers, createSupplier,
   updateSupplier, deleteSupplier 
 } = require("../controllers/SupplierControler")
+const { authorizedPermission } = require("../middlewares/authorizedPermission");
 
-router.get("/", getSuppliers)
-router.post("/", createSupplier)
-router.put("/:id", updateSupplier)
-router.delete("/:id", deleteSupplier)
+router.get("/", authorizedPermission(["AFFICHER", "AFFICHER_LIST"], "FOURNISSEURS"), getSuppliers)
+router.post("/", authorizedPermission(["AJOUTER"], "FOURNISSEURS"), createSupplier)
+router.put("/:id", authorizedPermission(["MODIFIER"], "FOURNISSEURS"), updateSupplier)
+router.delete("/:id", authorizedPermission(["SUPPRIMER"], "FOURNISSEURS"), deleteSupplier)
 
 module.exports = router
